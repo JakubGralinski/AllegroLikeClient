d4c36b76c7cbfae5144dc38f15461cf841fee580
mlaskowski7 <mtlaskowski7@gmail.com>
2025-04-23 20:05:07 +0200

    fix: fix buiild

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import Login from "./components/login/Login";
 import Protected from "./components/Protected";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
