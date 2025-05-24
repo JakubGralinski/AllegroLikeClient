447f23ec2e33fffd736c44f7f16f434266207916
mlaskowski7 <mtlaskowski7@gmail.com>
2025-05-25 00:05:33 +0200

    feat: implement address service

---
+++ src/lib/address.service.ts
@@ -0,0 +1,20 @@
+import {Address} from "./types.ts";
+import authService from "./auth.service.ts";
+import {BASE_API_URL} from "./constants.ts";
+import axios from "axios";
+
+class AddressService {
+    async searchAddress(searchQuery: string): Promise<Address[]> {
+        const jwtToken = authService.getCurrentUser();
+        const url = `${BASE_API_URL}/addresses/${searchQuery}`;
+        const response = await axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${jwtToken}`
+            }
+        });
+
+        return response.data;
+    }
+}
+
+export default new AddressService();
