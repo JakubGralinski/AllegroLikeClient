de50428145aaf4ed9846b5abad92cf6838055f93
mlaskowski7 <mtlaskowski7@gmail.com>
2025-05-24 23:07:14 +0200

    feat: adjust auth logic to new auth endpoints response structure

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,15 @@
 export type User = {
+  id: number;
   username: string;
+  email: string;
   role: string;
+  address: Address | null;
 };
+
+export type Address = {
+  id: number;
+  city: string;
+  country: string;
+  street: string;
+  houseNumber: number;
+}
