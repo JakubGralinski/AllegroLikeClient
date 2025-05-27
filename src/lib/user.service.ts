ab3d5eb13484adf050c54fb9094a11962029b67f
mlaskowski7 <mtlaskowski7@gmail.com>
2025-05-27 23:50:36 +0200

    feat: implement patch user endpoint call

--- src/lib/user.service.ts
+++ src/lib/user.service.ts
@@ -1,40 +1,67 @@
-import {User} from "./types.ts";
+import { User } from "./types.ts";
 import authService from "./auth.service.ts";
-import {BASE_API_URL} from "./constants.ts";
+import { BASE_API_URL } from "./constants.ts";
 import axios from "axios";
 
 export interface CreateCurrentUserAddressProps {
-    city: string,
-    country: string,
-    street: string,
-    houseNumber: number;
+  city: string;
+  country: string;
+  street: string;
+  houseNumber: number;
+}
+
+export interface UpdateCurrentUserProps {
+  username: string;
+  email: string;
 }
 
 class UserService {
-    async updateCurrentUserAddress(addressId: number, userId: number): Promise<User> {
-        const jwtToken = authService.getCurrentUser();
-        const url = `${BASE_API_URL}users/${userId}/address/${addressId}`;
-        const response = await axios.put(url, {
-            headers: {
-                Authorization: `Bearer ${jwtToken}`
-            }
-        });
+  async updateCurrentUserAddress(
+    addressId: number,
+    userId: number,
+  ): Promise<User> {
+    const jwtToken = authService.getCurrentUser();
+    const url = `${BASE_API_URL}users/${userId}/address/${addressId}`;
+    const response = await axios.put(url, {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    });
 
-        return response.data;
-    }
+    return response.data;
+  }
 
-    async createCurrentUserAddress(createUserAddressProps: CreateCurrentUserAddressProps, userId: number): Promise<User> {
-        const jwtToken = authService.getCurrentUser();
-        const url = `${BASE_API_URL}users/${userId}/address`;
-        const response = await axios.post(url, createUserAddressProps, {
-            headers: {
-                Authorization: `Bearer ${jwtToken}`,
-                'Content-Type': 'application/json',
-            }
-        });
+  async createCurrentUserAddress(
+    createUserAddressProps: CreateCurrentUserAddressProps,
+    userId: number,
+  ): Promise<User> {
+    const jwtToken = authService.getCurrentUser();
+    const url = `${BASE_API_URL}users/${userId}/address`;
+    const response = await axios.post(url, createUserAddressProps, {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        return response.data;
-    }
+    return response.data;
+  }
+
+  async updateCurrentUser(
+    updateCurrentUserProps: UpdateCurrentUserProps,
+    userId: number,
+  ): Promise<User> {
+    const jwtToken = authService.getCurrentUser();
+    const url = `${BASE_API_URL}users/${userId}`;
+    const response = await axios.patch(url, updateCurrentUserProps, {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  }
 }
 
 export default new UserService();
