7be735e18d85999e24c20f11312c01289a5bd514
mlaskowski7 <mtlaskowski7@gmail.com>
2025-05-28 23:44:01 +0200

    feat: add products slice to redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSliceReducer from "./auth";
 import cartReducer from "./cartSlice";
+import productsReducer from "./products";
 
 export const store = configureStore({
   reducer: {
     auth: authSliceReducer,
     cart: cartReducer,
+    products: productsReducer,
   },
 });
 
