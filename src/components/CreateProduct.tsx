e7b6a4ef19878bbec7fef356da3815a10adda381
mlaskowski7 <mtlaskowski7@gmail.com>
2025-06-03 19:41:12 +0200

    feat: implement category service, closes #33

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -6,21 +6,18 @@ import { useNavigate } from "react-router-dom";
 function CreateProduct(): ReactNode {
   const user = useSelector((state: RootState) => state.auth.user);
   const navigate = useNavigate();
-  
+
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [price, setPrice] = useState<number>(0);
   const [stockQuantity, setStockQuantity] = useState<number>(0);
-  
 
   if (user?.role !== "ROLE_ADMIN") {
     alert("You need ADMIN privileges to access create product page");
     navigate("/");
   }
-  
-  return (
-    
-  )
+
+  return <div></div>;
 }
 
 export default CreateProduct;
